37c3754c5b2758c6e415933386d06a417a55e43e
ksbaik <baik.kaishan@gmail.com>
2022-12-24 16:16:53 +0800

    Co-authored-by: Clemence Goh <clemencegoh@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,82 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import { Checkbox, Container, ListItem, ListItemButton, ListItemIcon, TextField, Typography } from '@mui/material';
+import { alpha, height } from '@mui/system';
+import styled from '@emotion/styled/types/base';
+import { ThemeContext } from '@emotion/react';
 
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/Inbox';
+
+//search MUI
+//ANT
+//
 function App() {
   const [count, setCount] = useState(0)
 
+  // const containerStyle = {
+  //   display: 'flex',
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  // };
+
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Container sx={{
+      
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      
+    }}>
+      
+
+      <Typography sx={{
+        color: 'blue',
+        display: 'block',
+      }}>Time to get shit done!</Typography>
+      <br></br>
+      
+      {/* <Box sx={{
+        width: '100%',
+        maxWidth: 360,
+        bgcolor: 'grey'
+      }}>
+        <List>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box> */}
+      
+
+
+      <ListItem sx={{
+        color: 'red'
+        }}>Item 1 
+        <Checkbox ></Checkbox>
+      </ListItem>
+      <ListItem sx={{
+        color: 'blue'
+        }}>Item 3 
+        <Checkbox ></Checkbox>
+      </ListItem>
+      <ListItem sx={{
+        color: 'green'
+        }}>Item 2 
+        <Checkbox ></Checkbox>
+      </ListItem>
+      
+    </Container>
   )
 }
 
